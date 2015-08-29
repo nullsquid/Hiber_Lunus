7aa38d0de48520fac47e15656136532c86b1a223
Azrael <j.shepard.ramsay@gmail.com>
2015-08-29 07:40:57 -0400

    initial commit

---
+++ Assets/TurnVariables.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class TurnVariables : NetworkBehaviour {
+	public int actionCount;
+	// Use this for initialization
+
+	[Command]
+	void CmdUseAction(){
+
+	}
+
+}
