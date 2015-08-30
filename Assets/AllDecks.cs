df2f88e82152148d2607797be2dabe74533ede5f
Hyacinth <j.shepard.ramsay@gmail.com>
2015-08-30 13:32:34 -0400

    wrote main character variables

---
+++ Assets/AllDecks.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AllDecks : MonoBehaviour {
+	// Use this for initialization
+	public Deck<Card> events = new Deck<Card>();
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Character.cs
+++ Assets/Character.cs
@@ -2,6 +2,40 @@
 using System.Collections;
 using System.Collections.Generic;
 public class Character : MonoBehaviour {
+	private int curActions;
+	private int bloodThreshold = 15;
+	private int fleshThreshold = 15;
+	private int boneThreshold = 15;
+	public Variant variant;
+	public List<Trait> traits = new List<Trait>();
+	public int maxActions;
+	public List<Card> bloodStack = new List<Card>();
+	public List<Card> fleshStack = new List<Card>();
+	public List<Card> boneStack = new List<Card>();
+
+	public int CurActions{
+		get{
+			return curActions;
+		}
+		set{
+			curActions = value;
+		}
+	}
+	public int BloodThreshold{
+		get{
+			return bloodThreshold;
+		}
+	}
+	public int FleshThreshold{
+			get{
+				return fleshThreshold;
+			}
+		}
+	public int BoneThreshold{
+		get{
+			return boneThreshold;
+		}
+	}
 
 	// Use this for initialization
 	void Start () {
